8ec5f6317d860bc26764d94a6f5b1b51caa0c4af
ElizabethMargott <proyecto.elizabethmargott@gmail.com>
2023-12-17 23:51:53 +0100

    Configuración de la base de datos

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'gf1gH6E5cDBCh5haHEEg2144-fBB-D-h',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '55864',
     }
 }
 
