182f71469bade99653acbd3f40fbfa7ef5620947
ElizabethMargott <proyecto.elizabethmargott@gmail.com>
2023-12-18 04:51:55 +0100

    f

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1&ygdjfj!z2qws*de^fqu(#n$%vya#%%afs+!kk4dg9zlpx!te'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
